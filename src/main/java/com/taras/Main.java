787b3f63462432ffe1b56690f8f2227ab15c7a3e
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-12-18 11:45:58 +0300

    2 commit

--- src/main/java/com/taras/Main.java
+++ src/main/java/com/taras/Main.java
@@ -1,5 +1,34 @@
 package com.taras;
 //mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart -DgroupId=com.taras -DartifactId=hibernate2 -Dversion=1.0.0
+
+/*create table author
+        (
+        id          int auto_increment
+        primary key,
+        last_name   varchar(25) null,
+        first_name  varchar(25) null,
+        middle_name varchar(25) null
+        );
+
+        create table book
+        (
+        id          int auto_increment
+        primary key,
+        title       varchar(255) null,
+        isbn        int(10)      null,
+        description text         null
+        );
+
+        create table book_authors
+        (
+        id        int(10) auto_increment
+        primary key,
+        book_id   int(10) null,
+        author_id int(10) null
+        );*/
+
+
+
 import java.sql.SQLException;
 import java.util.List;
 
